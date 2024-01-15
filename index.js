4c9d0d58e32764acba8b2d3f7ffdd4872e703eb5
dakotasnapshot <dakotacole@gmail.com>
2024-01-15 08:57:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,40 +5,45 @@ const renogy = require('./renogy');
 const logger = require('./logger');
 
 async function main() {
-
     logger.info('Starting NodeRenogy...');
 
     try {
         const args = cli.args;
-        logger.trace(args, 'With arguments...')
+        logger.trace(args, 'With arguments...');
         await renogy.begin();
 
         const controllerInfo = await renogy.getControllerInfo();
         logger.trace(controllerInfo, 'Controller Info...');
 
-        if(args.mqttbroker) {
+        if (args.mqttbroker) {
             await mqtt.publish(controllerInfo, 'device');
         }
 
-        setInterval(
-            async function() {
-                const result = await renogy.getData();   
+        setInterval(async () => {
+            try {
+                const result = await renogy.getData();
 
-                if(args.mqttbroker) {
+                if (args.mqttbroker) {
                     await mqtt.publish(result, 'state');
-                }
-                else {
+                } else {
                     logger.trace('No MQTT broker specified!');
                     console.log(result);
                 }
-            }, 
-            args.pollinginterval * 1000
-        );
-    }
-    catch(e) {
-        logger.error(e);
+            } catch (error) {
+                logger.error('Error in data polling:', error);
+                // Add more context or details if needed
+            }
+        }, args.pollinginterval * 1000);
+    } catch (error) {
+        logger.error('Error in main:', error);
         process.exit(1);
     }
 }
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Optionally, you can throw the error or handle it here.
+});
 
 main();
